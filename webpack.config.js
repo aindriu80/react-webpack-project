971bd24cf8bc30f24a3ddd6855ff1b2b95e256d8
aindriu <aindriu80@gmail.com>
2019-10-18 21:30:11 +0100

    lazy loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,48 @@
-const path = require("path");
+const path = require('path');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  devtool: "cheap-module-eval-source-map",
-  entry: "./src/index.js",
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: ""
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    chunkFilename: '[id].js',
+    publicPath: ''
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
-          { loader: "style-loader" },
+          { loader: 'style-loader' },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              importLoaders: 1,                  
+              importLoaders: 1,
               modules: true,
-              localIdentName: "[name]__[local]__[hash:base64:5]"
+              localIdentName: '[name]__[local]__[hash:base64:5]'
             }
           },
           {
-              loader: 'postcss-loader',
-              options: {                  
-                  ident: 'postcss',
-                  plugins: () =>[
-                      autoprefixer({
-                          browsers: [
-                            "> 1%",
-                            "last 2 versions"
-                          ]
-                      })
-                  ]
-              }
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: ['> 1%', 'last 2 versions']
+                })
+              ]
+            }
           }
         ]
       },
